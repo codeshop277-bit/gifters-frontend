e3a0687b5147d06ce522bfa775792838f03cc295
Codeshop277 <codeshop277@gmail.com>
2025-09-29 17:58:22 +0530

    RTK Query setup

---
+++ services/api.ts
@@ -0,0 +1,62 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { baseUrl } from "../utils/baseurl";
+import { RootState } from "./store";
+import { setCredentials } from "../store/authSlice";
+
+
+type LoginRequest = { email: string; password: string };
+type RegisterRequest = { name: string; email: string; password: string };
+type User = { id: string; name: string; email: string };
+type AuthResponse = { user: User; access_token: string };
+
+
+//createApi - creates the RTK query slice
+//reducerPath - key will be used in reducer
+//baseQuery - defines how every request is made.
+//prepareHeaders - adds header to all request
+export const api = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: baseUrl,
+        prepareHeaders: (headers, { getState}) => {
+            const token = (getState as any).auth.access_token
+            if (token) headers.set("Authorization", `Bearer ${token}`);
+            return headers
+        }
+    }),
+
+    //endpoints - each endpoint will automatically generate hooks  and manage states
+    //builder.mutation defines POST/PUT/DELETE
+    endpoints: (builder) => ({
+        login: builder.mutation<AuthResponse, LoginRequest>({
+            query: (credentials) => ({
+                url: "/users/login",
+                method: "POST",
+                body: credentials
+            }),
+            async onQueryStarted(arg, {dispatch, queryFulfilled}){
+                try{
+                    const { data } = await queryFulfilled;
+                    dispatch(setCredentials(data))
+                }catch {}
+            }
+        }),
+        register: builder.mutation<AuthResponse, RegisterRequest>({
+            query: (credentials) => ({
+                url: "users/register",
+                method: "POST",
+                body: credentials
+            }),
+            async onQueryStarted(arg, {dispatch, queryFulfilled}){
+                try{
+                    const { data } = await queryFulfilled;
+                    dispatch(setCredentials(data))
+                }catch {
+
+                }
+            }
+        })
+    })
+})
+
+export const { useLoginMutation, useRegisterMutation } = api;
--- services/store.ts
+++ services/store.ts
@@ -1,11 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../store/authSlice"
+import { api } from "./api";
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
-    }
+        auth: authReducer,
+        [api.reducerPath]: api.reducer
+    },
+    middleware: (getDefault) => getDefault().concat(api.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+// getdefault() handles the following 
+//     redux-thunk → allows async logic (like thunks).
+//     serializableStateInvariantMiddleware → warns if you put non-serializable values in state/actions (in dev).
+//     immutableStateInvariantMiddleware → warns if you accidentally mutate state (in dev).
+
+// api.middleware is generated automatically by RTK Query when you create your API slice (createApi).
+// It handles:
+//     Caching
+//     Deduping requests
+//     Auto re-fetching on focus/reconnect
+//     Keeping track of loading states
+//     Updating cached data when mutations succeed
