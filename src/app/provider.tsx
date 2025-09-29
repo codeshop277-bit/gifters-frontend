e29d4ecd80041f8bb4b0c70b035c61b49abf000a
Codeshop277 <codeshop277@gmail.com>
2025-09-29 17:26:21 +0530

    Use client

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,5 +1,7 @@
 "use client";
-
+// by default all components are treated as server side components in next js
+// to explicity set component to client rendering, we use "use client"
+//this allows us to use hooks and store
 import { Provider } from "react-redux";
 import { store } from "../../services/store";
 
